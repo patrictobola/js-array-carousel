8d4bccd9dc58a9eb5ac69db40e36e99582b5993c
patrictobola <tobola92@gmail.com>
2023-06-01 17:34:21 +0200

    setted thumbnails div in HTML-CSS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Recupero dal DOM l'elemento dove voglio inserire le foto
 const gallery = document.getElementById('gallery')
+// Recupero dal DOM l'elemento dove voglio inserire le foto del thumbnails
+const thumbnails = document.getElementById('thumbnails')
+
 // Recupero i bottoni da cliccare nel DOM 
 const prevBtn = document.getElementById('prevBtn')
 const nextBtn = document.getElementById('nextBtn')
@@ -12,7 +15,7 @@ const pictures = [
     "img/05.webp"
 ]
 
-// Mettiamo le immagini nel DOM 
+// Mettiamo le immagini nel DOM in gallery
 for (i = 0; i < pictures.length; i++){
     const imageElement = document.createElement('img');
     imageElement.src = pictures[i];
@@ -24,14 +27,19 @@ for (i = 0; i < pictures.length; i++){
 let currentIndex = 0;
 
 // Recupero tutte le immagini così da averle dentro un array 
-const images = document.querySelectorAll('img');
+const images = gallery.querySelectorAll('img');
 
 // Permetto alla (prima foto in questo caso) di essere visualizzata aggiungendo la classe active  
 images[currentIndex].classList.add('active'); 
 
 // Al click del bottone 'avanti'
 nextBtn.addEventListener('click', function(){
-    if (currentIndex < pictures.length - 1) {
+    if (currentIndex === pictures.length - 1) {
+        images[currentIndex].classList.remove('active');
+        currentIndex = 0;
+        images[currentIndex].classList.add('active');
+    }
+    else if (currentIndex < pictures.length){
         images[currentIndex].classList.remove('active'); 
         currentIndex ++;
         images[currentIndex].classList.add('active'); 
@@ -39,9 +47,25 @@ nextBtn.addEventListener('click', function(){
 })
 // Al click del bottone 'indetro'
 prevBtn.addEventListener('click', function(){
-    if (currentIndex > 0){
+    if (currentIndex === 0) {
+        images[currentIndex].classList.remove('active');
+        currentIndex = images.length - 1;
+        images[currentIndex].classList.add('active');
+    }
+    else if (currentIndex > 0){
         images[currentIndex].classList.remove('active'); 
         currentIndex -- ;
         images[currentIndex].classList.add('active'); 
     }
 })
+
+
+
+// Mettiamo le immagini nel DOM in thumbnails
+for (i = 0; i < pictures.length; i++){
+    const imageElement = document.createElement('img');
+    imageElement.src = pictures[i];
+    imageElement.alt = `Foto numero ${i + 1}`
+    imageElement.classList.add('active')
+    thumbnails.appendChild(imageElement)
+}
