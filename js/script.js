3399186dc73a994f929dd77fa9d7890b87bbdcc1
patrictobola <tobola92@gmail.com>
2023-06-01 17:56:38 +0200

    done. need minor fixes

--- js/script.js
+++ js/script.js
@@ -26,38 +26,12 @@ for (i = 0; i < pictures.length; i++){
 // Mi aiuto con un index 0 per visualizzare la prima foto ed eventualmente cambiare le altre
 let currentIndex = 0;
 
-// Recupero tutte le immagini così da averle dentro un array 
-const images = gallery.querySelectorAll('img');
+// Recupero tutte le immagini nella gallery così da averle dentro un array 
+const galleryImages = gallery.querySelectorAll('img');
 
 // Permetto alla (prima foto in questo caso) di essere visualizzata aggiungendo la classe active  
-images[currentIndex].classList.add('active'); 
+galleryImages[currentIndex].classList.add('active'); 
 
-// Al click del bottone 'avanti'
-nextBtn.addEventListener('click', function(){
-    if (currentIndex === pictures.length - 1) {
-        images[currentIndex].classList.remove('active');
-        currentIndex = 0;
-        images[currentIndex].classList.add('active');
-    }
-    else if (currentIndex < pictures.length){
-        images[currentIndex].classList.remove('active'); 
-        currentIndex ++;
-        images[currentIndex].classList.add('active'); 
-    }
-})
-// Al click del bottone 'indetro'
-prevBtn.addEventListener('click', function(){
-    if (currentIndex === 0) {
-        images[currentIndex].classList.remove('active');
-        currentIndex = images.length - 1;
-        images[currentIndex].classList.add('active');
-    }
-    else if (currentIndex > 0){
-        images[currentIndex].classList.remove('active'); 
-        currentIndex -- ;
-        images[currentIndex].classList.add('active'); 
-    }
-})
 
 
 
@@ -69,3 +43,45 @@ for (i = 0; i < pictures.length; i++){
     imageElement.classList.add('active')
     thumbnails.appendChild(imageElement)
 }
+
+// Recupero tutte le immagini nella gallery così da averle dentro un array 
+const thumbnailsImages = thumbnails.querySelectorAll('img');
+
+// Seleziono l'immagine al quale voglio applicare l'effetto
+thumbnailsImages[currentIndex].classList.add('selected')
+
+
+// Al click del bottone 'avanti'
+nextBtn.addEventListener('click', function(){
+    if (currentIndex === pictures.length - 1) {
+        galleryImages[currentIndex].classList.remove('active');
+        thumbnailsImages[currentIndex].classList.remove('selected');
+        currentIndex = 0;
+        galleryImages[currentIndex].classList.add('active');
+        thumbnailsImages[currentIndex].classList.add('selected');
+    }
+    else if (currentIndex < pictures.length){
+        galleryImages[currentIndex].classList.remove('active'); 
+        thumbnailsImages[currentIndex].classList.remove('selected'); 
+        currentIndex ++;
+        galleryImages[currentIndex].classList.add('active'); 
+        thumbnailsImages[currentIndex].classList.add('selected'); 
+    }
+})
+// Al click del bottone 'indetro'
+prevBtn.addEventListener('click', function(){
+    if (currentIndex === 0) {
+        galleryImages[currentIndex].classList.remove('active');
+        thumbnailsImages[currentIndex].classList.remove('selected');
+        currentIndex = galleryImages.length - 1;
+        galleryImages[currentIndex].classList.add('active');
+        thumbnailsImages[currentIndex].classList.add('selected');
+    }
+    else if (currentIndex > 0){
+        galleryImages[currentIndex].classList.remove('active'); 
+        thumbnailsImages[currentIndex].classList.remove('selected'); 
+        currentIndex -- ;
+        galleryImages[currentIndex].classList.add('active'); 
+        thumbnailsImages[currentIndex].classList.add('selected'); 
+    }
+})
